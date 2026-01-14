321fd395c63600b4c65701266be471f8bf4f13dc
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-14 21:04:39 +0530

    Scaffold & AppBar Widgets

--- 00.LabWork/06.Day19 (2026.01.07)/ict084/android/app/src/main/kotlin/com/example/ict084/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ict084
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ ex04_scaffold_appbar_widgets/android/app/src/main/kotlin/com/example/ex04_scaffold_appbar_widgets/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ex04_scaffold_appbar_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
