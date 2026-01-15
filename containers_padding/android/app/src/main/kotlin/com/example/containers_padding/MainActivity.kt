3db7f99ae2218ecb0e9574f6f1358073814a257f
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-16 01:00:08 +0530

    containers_padding

---
+++ containers_padding/android/app/src/main/kotlin/com/example/containers_padding/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.containers_padding
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ containers_padding/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home:Home()
+    )
+  );
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Demo App"),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
+        ),
+        body:Container(
+          padding: EdgeInsets.all(22.0),
+          color: Colors.amber,
+          child: Text("Hello"),
+        )
+      );
+  }
+}
