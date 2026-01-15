b96e5c2192470f1948705a8ec41f35cc0fa3cc7f
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-15 21:27:46 +0530

    Scaffold & AppBar Widgets

--- ex04_scaffold_appbar_widgets/lib/main.dart
+++ ex04_scaffold_appbar_widgets/lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home:Scaffold(
-      appBar: AppBar(
-        title: Text("Demo App"),
-        centerTitle: true,
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Demo App"),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text(
+            "My Name is Kalhara",
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("Button Clicked");
+          },
+          child: const Text("Click Me"),
+        ),
       ),
-      body: Text("My Name is Kalhara"),
-    ) ,
-  )
+    ),
   );
 }
-
