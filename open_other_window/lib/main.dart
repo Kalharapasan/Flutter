652c56f68e5ab21be26428f2630337fa97bc5001
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-17 20:34:10 +0530

    Open Other Screen (Navigator.push)

--- open_other_window/lib/main.dart
+++ open_other_window/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'second_screen.dart';
 
 void main() {
   runApp(const MyApp());
--- open_other_window/lib/second_screen.dart
+++ open_other_window/lib/second_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
 
