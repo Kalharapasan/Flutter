e117c24391c04619ea9ff91fbe79c2002cd6a7d7
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-17 20:14:30 +0530

    Basic EditText (TextField) Example

--- edittext/test/widget_test.dart
+++ edittext/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:compact_radio_group/main.dart';
+import 'package:edittext/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
