2cd8a0ab17c057d1ee96716d2383c4350a8ece03
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-17 16:30:53 +0530

    Expanded Widgets

--- expanded_widgets/test/widget_test.dart
+++ expanded_widgets/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:outline/main.dart';
+import 'package:expanded_widgets/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
