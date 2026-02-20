fe4f6d8330eeb7eac4b286fc5d6fb12bd03b4d28
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-02-20 23:59:45 +0530

    Test 08 in Exam Time

--- test08/lib/main.dart
+++ test08/lib/main.dart
@@ -1,3 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:test07/screen/addData.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
+  await Supabase.initialize(
+    url: 'https://dmqlgfdhpywddxkgwfkx.supabase.co',
+    anonKey: 'YOUR_ANON_KEY',
+  );
 
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AddDataScreen(),
+    );
+  }
+}
